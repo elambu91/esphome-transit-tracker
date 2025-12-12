db79ae8b333e6a7f261ffce2ff0a03343829349c
Elam Buteil <elam.buteil@groundcover.com>
2025-12-12 18:26:16 +0200

    fix(): main - fix reversal

--- components/transit_tracker/string_utils.cpp
+++ components/transit_tracker/string_utils.cpp
@@ -1,6 +1,7 @@
 #include "string_utils.h"
 #include <sstream>
 #include <algorithm>
+#include <vector>
 
 std::vector<std::string> split(const std::string &s, char delim) {
   std::vector<std::string> elems;
@@ -13,7 +14,36 @@ std::vector<std::string> split(const std::string &s, char delim) {
 }
 
 std::string reverse_string(const std::string &s) {
-  std::string result = s;
-  std::reverse(result.begin(), result.end());
+  std::vector<std::string> chars;
+  size_t i = 0;
+  
+  while (i < s.length()) {
+    unsigned char c = s[i];
+    size_t char_len = 1;
+    
+    if ((c & 0x80) == 0) {
+      char_len = 1;
+    } else if ((c & 0xE0) == 0xC0) {
+      char_len = 2;
+    } else if ((c & 0xF0) == 0xE0) {
+      char_len = 3;
+    } else if ((c & 0xF8) == 0xF0) {
+      char_len = 4;
+    }
+    
+    if (i + char_len <= s.length()) {
+      chars.push_back(s.substr(i, char_len));
+    }
+    
+    i += char_len;
+  }
+  
+  std::reverse(chars.begin(), chars.end());
+  
+  std::string result;
+  for (const auto &ch : chars) {
+    result += ch;
+  }
+  
   return result;
 }
